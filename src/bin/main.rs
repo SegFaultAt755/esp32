5aa3a04cfe34437e555308a94de8c0be4d8f7b86
UserWithARegularName <segfaultat755@gmail.com>
2026-09-24 11:36:31 +0300

    Initial Commit

---
+++ src/bin/main.rs
@@ -0,0 +1,43 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+#![deny(clippy::large_stack_frames)]
+
+use esp_backtrace as _;
+use esp_hal::clock::CpuClock;
+use esp_hal::main;
+use esp_hal::time::{Duration, Instant};
+
+extern crate alloc;
+
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[allow(
+    clippy::large_stack_frames,
+    reason = "it's not unusual to allocate larger buffers etc. in main"
+)]
+#[main]
+fn main() -> ! {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let _gpio6 = peripherals.GPIO6;
+    let _gpio7 = peripherals.GPIO7;
+    let _gpio8 = peripherals.GPIO8;
+    let _gpio9 = peripherals.GPIO9;
+    let _gpio10 = peripherals.GPIO10;
+    let _gpio11 = peripherals.GPIO11;
+    let _gpio16 = peripherals.GPIO16;
+    let _gpio20 = peripherals.GPIO20;
+
+    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 98768);
+
+    loop {
+        let delay_start = Instant::now();
+        while delay_start.elapsed() < Duration::from_millis(500) {}
+    }
+}
